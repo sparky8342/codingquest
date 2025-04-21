54e70fffdbba053fcdd7f11dabccfb9bfa31974a
David Bell <david.bell@gmail.com>
2025-04-21 04:00:32 +0100

    refactor neighbours

--- 2022/9.c
+++ 2022/9.c
@@ -13,7 +13,7 @@ typedef struct {
 int width;
 int height;
 
-void scan(char grid[height][width], Pos *start, Pos *end) {
+void find_start_end(char grid[height][width], Pos *start, Pos *end) {
     for (int x = 0; x < width; x++) {
         if (grid[0][x] == ' ') {
             start->x = x;
@@ -34,6 +34,7 @@ int main() {
         return 1;
     }
 
+    // get size of grid
     char buffer[1000];
     fgets(buffer, sizeof(buffer), fptr);
     for (int i = 0; i < sizeof(buffer); i++) {
@@ -51,6 +52,7 @@ int main() {
     }
     rewind(fptr);
 
+    // create and read in grid
     char grid[height][width];
     bool visited[height][width];
     for (int y = 0; y < height; y++) {
@@ -68,12 +70,14 @@ int main() {
     start->steps = 1;
     Pos *end = (Pos *)malloc(sizeof(Pos));
 
-    scan(grid, start, end);
+    find_start_end(grid, start, end);
 
     // TODO look at using a linked list queue instead
     Pos **queue = NULL;
     cvector_push_back(queue, start);
     visited[start->y][start->x] = true;
+
+    static int dirs[] = {0, 1, 0, -1, 1, 0, -1, 0};
 
     while (cvector_size(queue) > 0) {
         Pos *pos = *cvector_at(queue, 0);
@@ -84,43 +88,30 @@ int main() {
             break;
         }
 
-        if (pos->y > 0 && grid[pos->y - 1][pos->x] == ' ' &&
-            visited[pos->y - 1][pos->x] == false) {
+        for (int i = 0; i < 8; i += 2) {
+            int next_x = pos->x + dirs[i];
+            int next_y = pos->y + dirs[i + 1];
+            if (next_x < 0 || next_x == width || next_y < 0 ||
+                next_y == height) {
+                continue;
+            }
+            if (grid[next_y][next_x] == '#') {
+                continue;
+            }
+            if (visited[next_y][next_x] == true) {
+                continue;
+            }
+
             Pos *next = (Pos *)malloc(sizeof(Pos));
-            next->x = pos->x;
-            next->y = pos->y - 1;
-            next->steps = pos->steps + 1;
-            cvector_push_back(queue, next);
-            visited[next->y][next->x] = true;
-        }
-        if (pos->y < height - 1 && grid[pos->y + 1][pos->x] == ' ' &&
-            visited[pos->y + 1][pos->x] == false) {
-            Pos *next = (Pos *)malloc(sizeof(Pos));
-            next->x = pos->x;
-            next->y = pos->y + 1;
-            next->steps = pos->steps + 1;
-            cvector_push_back(queue, next);
-            visited[next->y][next->x] = true;
-        }
-        if (pos->x > 0 && grid[pos->y][pos->x - 1] == ' ' &&
-            visited[pos->y][pos->x - 1] == false) {
-            Pos *next = (Pos *)malloc(sizeof(Pos));
-            next->x = pos->x - 1;
-            next->y = pos->y;
-            next->steps = pos->steps + 1;
-            cvector_push_back(queue, next);
-            visited[next->y][next->x] = true;
-        }
-        if (pos->x < width - 1 && grid[pos->y][pos->x + 1] == ' ' &&
-            visited[pos->y][pos->x + 1] == false) {
-            Pos *next = (Pos *)malloc(sizeof(Pos));
-            next->x = pos->x + 1;
-            next->y = pos->y;
+            next->x = next_x;
+            next->y = next_y;
             next->steps = pos->steps + 1;
             cvector_push_back(queue, next);
             visited[next->y][next->x] = true;
         }
     }
+
+    cvector_free(queue);
 
     return 0;
 }
