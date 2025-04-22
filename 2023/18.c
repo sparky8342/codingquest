4abf9dd9c912b01d2dca1df82559d531a1c15782
David Bell <david.bell@gmail.com>
2025-04-22 16:14:24 +0100

    replaced fgets with fscanf

--- 2023/18.c
+++ 2023/18.c
@@ -13,35 +13,9 @@ int main() {
     cvector_vector_type(char *) categories = NULL;
     int **amounts = NULL;
 
-    char buffer[100];
-    while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
-        int amount = 0;
-
-        int spaces = 0;
-        int str_start;
-        int str_end;
-        for (int i = 0; i < sizeof(buffer); i++) {
-            if (buffer[i] == '\n') {
-                str_end = i - 1;
-                break;
-            } else if (buffer[i] == ' ') {
-                spaces++;
-                if (spaces == 2) {
-                    str_start = i + 1;
-                }
-            } else if (spaces == 1) {
-                amount = amount * 10 + buffer[i] - '0';
-            }
-        }
-        int len = str_end - str_start + 2;
-        char category[len];
-        int cat_i = 0;
-        for (int i = str_start; i <= str_end; i++) {
-            category[cat_i] = buffer[i];
-            cat_i++;
-        }
-        category[cat_i] = '\0';
-
+    char category[100];
+    int amount;
+    while (fscanf(fptr, "%s %d %s", category, &amount, category) == 3) {
         int category_no = -1;
         for (int i = 0; i < cvector_size(categories); i++) {
             if (strncmp(categories[i], category, sizeof(category)) == 0) {
