e06c28a2ab193a1796b9c1adb7f6f96e2d515bc2
David Bell <david.bell@gmail.com>
2025-04-22 16:06:02 +0100

    fscanf instead of fgets

--- 2023/19.c
+++ 2023/19.c
@@ -22,22 +22,14 @@ int main() {
     int total = 0;
     int amount = 0;
 
-    char buffer[100];
-    while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
-        int n = 0;
-        for (int i = 0; i < sizeof(buffer); i++) {
-            if (buffer[i] == '\n') {
-                break;
-            }
-            n = n * 10 + buffer[i] - '0';
-        }
+    int n;
+    while (fscanf(fptr, "%d", &n) == 1) {
         if ((bitcount(n) & 1) == 0) {
             n &= 0b111111111111111;
             total += n;
             amount++;
         }
     }
-
     fclose(fptr);
 
     float avg = (float)total / (float)amount;
