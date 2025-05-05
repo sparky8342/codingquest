9b482df4b1d26de66a87957ceaf16bb11a5c4fce
David Bell <david.bell@gmail.com>
2025-05-05 17:15:19 +0100

    removed extra code

--- 2022_practice/15.c
+++ 2022_practice/15.c
@@ -46,11 +46,6 @@ int main() {
 
     // create and read in grid
     int grid[height][width];
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-        }
-    }
-
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
             fscanf(fptr, "%d", &grid[row][col]);
